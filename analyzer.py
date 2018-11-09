18f33db169a91573c8d00f314a496dc97f6aaa92
Nicholas Jones <dojoman19@gmail.com>
2018-11-09 16:32:54 -0500

    Added argparser

--- analyzer.py
+++ analyzer.py
@@ -1,11 +1,9 @@
 import requests, sys, random, argparse
 
-#TODO: add argparser 
-
-def main(iterations):
+def main(initial, topics, iterations):
     seen_words = ['hack']
-    base_format_url = "https://api.datamuse.com/words?rel_trg={}&topics=computer,hack,mechanics"
-    start_url = base_format_url.format("hack")
+    base_format_url = "https://api.datamuse.com/words?rel_trg={}&topics={}"
+    start_url = base_format_url.format(initial, topics)
     response = requests.get(start_url)
     resp_json = response.json()
     curr_ind = random.randint(0, 3)
@@ -15,9 +13,8 @@ def main(iterations):
         potential = resp_json[curr_ind]['word']
     else:
         seen_words.append(potential)
-    print(potential)
     for _ in range(0, iterations):
-        curr_url = base_format_url.format(potential)
+        curr_url = base_format_url.format(potential, topics)
         response = requests.get(curr_url)
         resp_json = response.json()
         curr_ind = random.randint(0, 3)
@@ -27,8 +24,15 @@ def main(iterations):
             potential = resp_json[curr_ind]['word']
         else:
             seen_words.append(potential)
-        print(potential)
+    print(seen_words)
 
 if __name__ == '__main__':
-    iterations = int(sys.argv[1])
-    main(iterations)
+    main_parser = argparse.ArgumentParser()
+    main_parser.add_argument('--initial', '-s', required=True, help="Initial word to start with")
+    main_parser.add_argument('--topics', '-t', nargs=1, required=True,
+                             help="Comma separated topic list to make " \
+                                  "sure the words are roughly related to")
+    main_parser.add_argument('--iterations', '-i', required=True, type=int,
+                             help="Number of iterations to go down")
+    args = main_parser.parse_args()
+    main(args.initial, args.topics, args.iterations)
