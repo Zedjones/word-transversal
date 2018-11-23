99f8008d425e924e6dc1706c956d28efc2f75305
Nicholas Jones <dojoman19@gmail.com>
2018-11-23 17:51:31 -0500

    Did some cleanup to prevent plural/singular duplicates

--- analyzer.py
+++ analyzer.py
@@ -32,14 +32,6 @@ def random_iteration(initial, topics, iterations):
 def layered_iteration(initial, topics, root, level, max_depth, seen_words, level_words, word_dict):
     if level == max_depth:
         return None
-    seen_words.append(initial)
-    if initial.endswith('y'):
-        seen_words.append(initial[:-1] + "ies")
-    if initial.endswith('ies'):
-        seen_words.append(initial[:-3] + "y")
-    if initial.endswith('s'):
-        seen_words.append(initial[:-1])
-    seen_words.append(initial + "s")
     url = BASE_FORMAT_URL.format(initial, topics)
     response = requests.get(url)
     synonyms = response.json()
@@ -57,6 +49,16 @@ def layered_iteration(initial, topics, root, level, max_depth, seen_words, level
             word = synonyms[curr_ind]['word']
         word_dict[word] = Node(word, parent=root)
         seen_words.append(word)
+        if word.endswith('y'):
+            seen_words.append(word[:-1] + "ies")
+        if word.endswith('ies'):
+            seen_words.append(word[:-3] + "y")
+        if word.endswith('s'):
+            seen_words.append(word[:-1])
+        if word.endswith('es'):
+            seen_words.append(word[:-2])
+        seen_words.append(word + "s")
+        seen_words.append(word + "es")
         new_words.append(word)
         added += 1
     for word in new_words:
@@ -68,11 +70,28 @@ if __name__ == '__main__':
     main_parser.add_argument('--initial', '-s', default="hack", help="Initial word to start with")
     main_parser.add_argument('--topics', '-t', nargs=1, default="technology",
                              help="Comma separated topic list to make " \
-                                  "sure the words are roughly related to")
+                                  "sure the words are roughly related to. " \
+                                  "Type 'None' for no topics")
     main_parser.add_argument('--iterations', '-i', required=True, type=int,
-                             help="Number of iterations to go down")
+                             help="Number of iterations/depth to go down")
+    main_parser.add_argument('--breadth', '-b', required=False, default=1,
+                             help="Number of related words on each depth level")
     args = main_parser.parse_args()
     root = Node(args.initial)
-    layered_iteration(args.initial, args.topics, root, 0, args.iterations, [], 2, {})
+    if args.topics == 'None':
+        BASE_FORMAT_URL = "https://api.datamuse.com/words?rel_trg={}"
+    seen_words = [args.initial]
+    if args.initial.endswith('y'):
+        seen_words.append(args.initial[:-1] + "ies")
+    if args.initial.endswith('ies'):
+        seen_words.append(args.initial[:-3] + "y")
+    if args.initial.endswith('s'):
+        seen_words.append(args.initial[:-1])
+    if args.initial.endswith('es'):
+        seen_words.append(args.initial[:-2])
+    seen_words.append(args.initial + "s")
+    seen_words.append(args.initial + "es")
+    layered_iteration(args.initial, args.topics, root, 0, args.iterations, 
+                      seen_words, int(args.breadth), {})
     print(RenderTree(root))
     DotExporter(root).to_picture("./root.png")
