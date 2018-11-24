f22c2b234541a84284eff963785fd71f0dc011ea
Nicholas Jones <dojoman19@gmail.com>
2018-11-24 15:08:00 -0500

    Got rid of the old function

--- analyzer.py
+++ analyzer.py
@@ -4,31 +4,6 @@ from anytree.exporter import DotExporter
 
 BASE_FORMAT_URL = "https://api.datamuse.com/words?rel_trg={}&topics={}"
 
-def random_iteration(initial, topics, iterations):
-    seen_words = [initial]
-    start_url = BASE_FORMAT_URL.format(initial, topics)
-    response = requests.get(start_url)
-    resp_json = response.json()
-    curr_ind = random.randint(0, 3)
-    potential = resp_json[curr_ind]['word']
-    while potential in seen_words:
-        curr_ind += 1
-        potential = resp_json[curr_ind]['word']
-    else:
-        seen_words.append(potential)
-    for _ in range(0, iterations):
-        curr_url = BASE_FORMAT_URL.format(potential, topics)
-        response = requests.get(curr_url)
-        resp_json = response.json()
-        curr_ind = random.randint(0, 3)
-        potential = resp_json[curr_ind]['word']
-        while potential in seen_words:
-            curr_ind += 1
-            potential = resp_json[curr_ind]['word']
-        else:
-            seen_words.append(potential)
-    print(seen_words)
-
 def layered_iteration(initial, topics, root, level, max_depth, seen_words, level_words, word_dict):
     if level == max_depth:
         return None
