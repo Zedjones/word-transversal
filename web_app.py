843318934fd378677928a4776e76a04d635353d8
Nicholas Jones <dojoman19@gmail.com>
2018-12-09 23:05:49 -0500

    Much closer to have the form fully working

--- web_app.py
+++ web_app.py
@@ -1,22 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import Form, StringField, IntegerField, FieldList, FormField, validators
-from flask_bootstrap import Bootstrap 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'CDB14257595C9E3B85884E0E366A63267C4980CD5A0AC2395C05CD1591692037'
-Bootstrap(app)
 
 class MainForm(FlaskForm):
     initial_word = StringField('Initial Word')
     iterations = IntegerField('Iterations/Depth')
-    breadth = IntegerField('Width/Breadth')
+    breadth = IntegerField('Breadth/Width')
     topic1 = StringField('Topic 1')
     topic2 = StringField('Topic 2')
     topic3 = StringField('Topic 3')
     topic4 = StringField('Topic 4')
     topic5 = StringField('Topic 5')
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    form = MainForm()
-    return render_template("index.html", form=form)
+    form = MainForm(request.form)
+    print(form.initial_word)
+    print(form.iterations)
+    print(form.breadth)
+    print(form.topic1)
+    print(form.topic2)
+    print(form.topic3)
+    print(form.topic4)
+    print(form.topic5)
+    return render_template("index.html")
